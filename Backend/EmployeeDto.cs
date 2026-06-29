ed9c60fd6dfa07d3d0080266b6c3010459681f37
Mona24532 <148537787+Mona24532@users.noreply.github.com>
2026-06-29 09:28:03 +0200

    Backend- EmployeeDto ,appsettings.json, update

---
+++ Backend/EmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend
+{
+    public class EmployeeDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Passsword { get; set; }
+        [Required]
+        public int Salary { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [Required]
+        public int Position_id { get; set; }
+    }
+}
