885100d934c18c2c14eb7135ce9fea24e156a97f
Mona24532 <148537787+Mona24532@users.noreply.github.com>
2026-06-29 10:00:42 +0200

    Backend- JWTService, Token authentication update

--- Backend/JWTService.cs
+++ Backend/JWTService.cs
@@ -1,7 +1,32 @@
-﻿namespace Backend
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend
 {
     public class JWTService
     {
+        private readonly IConfiguration _config;
+        public JWTService(IConfiguration config)
+        {
+            _config = config;
+        }
 
+        public string GenerateToken()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,9 @@
 using Backend;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +17,33 @@ builder.Services.AddDbContext<DBConfig>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<JWTService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]);
+        var issuer = builder.Configuration["Jwt:Issuer"];
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine(context.Exception.Message);
+                return Task.CompletedTask;
+            }
+        };
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidIssuer = builder.Configuration["Jwt:Issuer"]
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
