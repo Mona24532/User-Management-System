7165fe237fad8820f1190afa81817e76885fc5f5
Mona24532 <148537787+Mona24532@users.noreply.github.com>
2026-07-02 08:57:41 +0200

    Frontend- AdminLogin, LoginModel Validation update

--- Frontend/LoginModel.cs
+++ Frontend/LoginModel.cs
@@ -4,10 +4,12 @@ namespace Frontend
 {
     public class LoginModel
     {
-   
-        
+
+        [Required(ErrorMessage ="Email megadása kötelező")]
+        [EmailAddress(ErrorMessage ="Email formátuma hibás")]
         public string Email { get; set; }
-       
+
+        [Required(ErrorMessage ="Jelszó megadása kötelező")]
         public string Password { get; set; }      
        // public string Role { get; set; }
         
